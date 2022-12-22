ab60afb65f9179894f869dd7f93bf03fdb6beb7a
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-12-22 16:04:15 +0100

    add donation route

--- web-app/src/App/App.tsx
+++ web-app/src/App/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Donation from '../pages/Donation/Donation';
+import { DONATION_PATH } from '../pages/paths';
+
 function App() {
-  return <div>Wild carbon</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path={DONATION_PATH} element={<Donation />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
