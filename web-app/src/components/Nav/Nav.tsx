ca3d9c55421f70b044e09e9c23225dc862197744
JOUANNE <quentin.jouanne94@gmail.com>
2023-01-26 12:56:01 +0100

    fixed nav bottom

--- web-app/src/components/Nav/Nav.tsx
+++ web-app/src/components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Test from '../Test';
 
 const Navigation = () => {
   const Menus = [
@@ -9,35 +10,38 @@ const Navigation = () => {
   ];
   const [active, setActive] = useState(0);
   return (
-    <div className="bg-[#484B8A] max-h-[5rem] pr-6 rounded-t-xl ">
-      <ul className="flex justify-around">
-        {Menus.map((menu, i) => (
-          <li key={i} className="w-16">
-            <a
-              className="flex flex-col text-center pt-6"
-              onClick={() => setActive(i)}
-            >
-              <span
-                className={`text-xl cursor-pointer duration-500 ${
-                  i === active && '-mt-6 text-white'
-                }`}
+    <>
+      <Test />
+      <nav className="bg-[#484B8A] max-h-[5rem] pr-6 rounded-t-xl fixed bottom-0 w-full z-50">
+        <ul className="flex justify-around">
+          {Menus.map((menu, i) => (
+            <li key={i} className="w-16">
+              <a
+                className="flex flex-col text-center pt-6"
+                onClick={() => setActive(i)}
               >
-                {menu.icon}
-              </span>
-              <span
-                className={` ${
-                  active === i
-                    ? 'translate-y-4 duration-700 opacity-100 text-white'
-                    : 'opacity-0 translate-y-10  '
-                } `}
-              >
-                {menu.name}
-              </span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+                <span
+                  className={`text-xl cursor-pointer duration-500 ${
+                    i === active && '-mt-6 text-white'
+                  }`}
+                >
+                  {menu.icon}
+                </span>
+                <span
+                  className={` ${
+                    active === i
+                      ? 'translate-y-4 duration-700 opacity-100 text-white'
+                      : 'opacity-0 translate-y-10  '
+                  } `}
+                >
+                  {menu.name}
+                </span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 };
 
