d714f752c9a3ee68934db8ed4ff6a1b07d29d9cc
JOUANNE <quentin.jouanne94@gmail.com>
2023-01-26 16:01:24 +0100

    setup route + nav

--- web-app/src/components/Nav/Nav.tsx
+++ web-app/src/components/Nav/Nav.tsx
@@ -1,28 +1,50 @@
-import { useState } from 'react';
-import Test from '../Test';
+import { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineEuroCircle,
+} from 'react-icons/ai';
+import { BiGitCompare } from 'react-icons/bi';
+import { HOME_PATH, DONATION_PATH } from '../../pages/paths';
+
+interface MenuItem {
+  name: string;
+  icon: JSX.Element;
+  path: string;
+}
 
 const Navigation = () => {
-  const Menus = [
-    { name: 'Accueil', icon: 'Home' },
-    { name: 'Profil', icon: 'Profile' },
-    { name: 'Contact', icon: 'Friends' },
-    { name: 'Donation', icon: 'Donation' },
+  const Menus: MenuItem[] = [
+    { name: 'Accueil', icon: <AiOutlineHome />, path: HOME_PATH },
+    { name: 'Profil', icon: <AiOutlineUser />, path: '/' },
+    { name: 'Comparer', icon: <BiGitCompare />, path: '/' },
+    { name: 'Donner', icon: <AiOutlineEuroCircle />, path: DONATION_PATH },
   ];
   const [active, setActive] = useState(0);
+
+  let location = useLocation();
+  useEffect(() => {
+    Menus.forEach((menu, i) => {
+      if (location.pathname === menu.path) {
+        setActive(i);
+      }
+    });
+  }, [location]);
   return (
     <>
-      <Test />
       <nav className="bg-[#484B8A] max-h-[5rem] pr-6 rounded-t-xl fixed bottom-0 w-full z-50">
         <ul className="flex justify-around">
           {Menus.map((menu, i) => (
             <li key={i} className="w-16">
-              <a
-                className="flex flex-col text-center pt-6"
+              <Link
+                className="flex flex-col text-center pt-7 text-white"
+                to={menu.path}
                 onClick={() => setActive(i)}
               >
                 <span
-                  className={`text-xl cursor-pointer duration-500 ${
-                    i === active && '-mt-6 text-white'
+                  className={`text-[30px] cursor-pointer duration-500 flex justify-center ${
+                    i === active && '-mt-4 '
                   }`}
                 >
                   {menu.icon}
@@ -30,13 +52,13 @@ const Navigation = () => {
                 <span
                   className={` ${
                     active === i
-                      ? 'translate-y-4 duration-700 opacity-100 text-white'
+                      ? 'translate-y-1 duration-700 opacity-100 text-white'
                       : 'opacity-0 translate-y-10  '
                   } `}
                 >
                   {menu.name}
                 </span>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
--- web-app/src/components/Test.tsx
+++
@@ -1,5 +0,0 @@
-const Test = () => {
-  return <div>Enter</div>;
-};
-
-export default Test;
