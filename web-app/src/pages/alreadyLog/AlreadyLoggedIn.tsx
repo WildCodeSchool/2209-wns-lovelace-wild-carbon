a2292cd37af524891f08e302b7101078d6a26566
JOUANNE <quentin.jouanne94@gmail.com>
2023-03-02 16:50:31 +0100

    remove dead code

--- web-app/src/pages/alreadyLog/AlreadyLoggedIn.tsx
+++ web-app/src/pages/alreadyLog/AlreadyLoggedIn.tsx
@@ -1,26 +1,5 @@
-// import React from 'react';
-// import { Navigate } from 'react-router-dom';
-// import { toast } from 'react-toastify';
-// import { HOME_PATH } from '../paths';
-
-// type PropsType = {
-//   isLoggedIn: boolean;
-//   children: any;
-// };
-// const AlreadyLoggedIn = (props: PropsType) => {
-//   const { isLoggedIn, children } = props;
-//   if (isLoggedIn) {
-//     toast.error('Vous devez être connecté pour acceder à cette page', {
-//       position: toast.POSITION.BOTTOM_RIGHT,
-//       toastId: 'alreadyLoggedIn',
-//     });
-//     return <></>;
-//   }
-//   return children;
-// };
-// export default AlreadyLoggedIn;
 const AlreadyLoggedIn = () => {
-  return <div>Enter</div>;
+  return <div></div>;
 };
 
 export default AlreadyLoggedIn;
