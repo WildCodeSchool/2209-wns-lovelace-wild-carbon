5889caaa4af30005215ed5eafaaab61896c78039
RayaneArroudj <rayane.arroudj@gmail.com>
2023-03-29 15:24:51 +0200

    Do not clear tables at server startup in production

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -13,6 +13,7 @@ import AppUserResolver from './resolvers/AppUser/AppUser.resolver';
 import AppUserRepository from './models/AppUser/AppUser.repository';
 import { getSessionIdInCookie } from './http-utils';
 import AppUser from './models/AppUser/AppUser.entity';
+import { IS_PRODUCTION } from './config';
 
 export type GlobalContext = ExpressContext & {
   user: AppUser | null;
@@ -49,9 +50,11 @@ const startServer = async () => {
   // The `listen` method launches a web server.
   const { url } = await server.listen();
   await initializeDatabaseRepositories();
-  await CategoryRepository.initializeCategories();
-  await SpendingRepository.initializeSpending();
-  await ArticleRepository.initializeArticles();
+  if (!IS_PRODUCTION) {
+    await CategoryRepository.initializeCategories();
+    await SpendingRepository.initializeSpending();
+    await ArticleRepository.initializeArticles();
+  }
 
   console.log(`🚀  Server ready at ${url}`);
 };
