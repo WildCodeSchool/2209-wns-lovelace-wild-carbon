eb074f132b6c96c7bb415e231ff900c12b47d06c
Quentin.J <quentin.jouanne94@gmail.com>
2023-05-25 16:55:49 +0200

    setup front with no css and add function like contributors, total amount and selectedAmount

--- web-app/src/pages/Donation/Donation.tsx
+++ web-app/src/pages/Donation/Donation.tsx
@@ -1,13 +1,89 @@
-import { MdConstruction } from 'react-icons/md';
+import { useState } from 'react';
 
 const Donation = () => {
+  const [total, setTotal] = useState<number>(0);
+  const [selectedAmount, setSelectedAmount] = useState<string>('');
+  const [contributions, setContributions] = useState<string[]>([]);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleButtonClick = (amountToAdd: number) => {
+    setSelectedAmount(amountToAdd.toString());
+  };
+
+  const handleConfirmClick = () => {
+    const amountToAdd = parseInt(selectedAmount);
+    if (!isNaN(amountToAdd)) {
+      setTotal(total + amountToAdd);
+      setSelectedAmount('');
+      setContributions([...contributions, amountToAdd.toString()]);
+    }
+  };
+
+  const lastContributors = contributions
+    .slice(-4)
+    .map((contribution, index) => <p key={index}>{contribution}€</p>);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
   return (
-    <div className="font-bold items-center flex justify-center flex-col mt-[40%] italic text-center ">
-      Vous ne pouvez pas participer au projet pour le moment.
-      <br /> Patience!!
-      <MdConstruction
-        style={{ fontSize: '60px', color: 'orange', marginTop: '10px' }}
-      />
+    <div>
+      <div>
+        <h1>Donation</h1>
+        <p>Soutenez Wild Carbon !</p>
+      </div>
+      <div>
+        <h3>Total de la cagnotte en cours : </h3>
+        <p>{total}€</p>
+      </div>
+      <div></div>
+      <div>
+        <h2>Je participe à hauteur de :</h2>
+        <input
+          type="text"
+          placeholder="142758"
+          value={selectedAmount}
+          onChange={(e) => setSelectedAmount(e.target.value)}
+        />
+        <span>€</span>
+      </div>
+      <div>
+        <button type="submit" onClick={() => handleButtonClick(5)}>
+          5€
+        </button>
+        <button type="submit" onClick={() => handleButtonClick(10)}>
+          10€
+        </button>
+        <button type="submit" onClick={() => handleButtonClick(20)}>
+          20€
+        </button>
+      </div>
+      <div>
+        <button type="submit" onClick={handleConfirmClick}>
+          Confirmer
+        </button>
+      </div>
+      <div>
+        <h4>Derniers contributeurs :</h4>
+        {lastContributors}
+        <button type="button" onClick={openModal}>
+          Voir l'historique complet
+        </button>
+      </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowModal(false)}>
+              &times;
+            </span>
+            <h4>Historique des contributions :</h4>
+            {contributions.map((contribution, index) => (
+              <p key={index}>{contribution}€</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
