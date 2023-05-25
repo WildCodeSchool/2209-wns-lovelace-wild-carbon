780d074c90399828d4c9066040f1ff1c0430f3ad
Quentin.J <quentin.jouanne94@gmail.com>
2023-05-25 17:45:14 +0200

    add user

--- back-end/src/models/Donation/Donation.repository.ts
+++ back-end/src/models/Donation/Donation.repository.ts
@@ -1,14 +1,19 @@
+import AppUser from '../AppUser/AppUser.entity';
 import DonationDb from './Donation.db';
 import Donation from './Donation.entity';
 
 export default class DonationRepository extends DonationDb {
   static async getDonations(): Promise<Donation[]> {
-    return this.repository.find();
+    return this.repository.find({ relations: { user: true } });
   }
 
-  static async createDonation(amount: number): Promise<Donation> {
+  static async createDonation(
+    amount: number,
+    user: AppUser
+  ): Promise<Donation> {
     const newDonation = this.repository.create({
       amount,
+      user,
     });
     await this.repository.save(newDonation);
     return newDonation;
