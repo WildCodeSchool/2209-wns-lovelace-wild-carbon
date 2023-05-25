26e2adb0a729f3b269ab7dd6a2a95349f08bde70
Quentin.J <quentin.jouanne94@gmail.com>
2023-05-25 17:45:38 +0200

    seting context

--- back-end/src/resolvers/Donation/Donation.resolver.ts
+++ back-end/src/resolvers/Donation/Donation.resolver.ts
@@ -3,6 +3,8 @@ import { Arg, Authorized, Ctx, Mutation, Query, Resolver } from 'type-graphql';
 // import AppUser from '../../models/AppUser/AppUser.entity';
 import DonationRepository from '../../models/Donation/Donation.repository';
 import Donation from '../../models/Donation/Donation.entity';
+import { GlobalContext } from '../..';
+import AppUser from '../../models/AppUser/AppUser.entity';
 
 @Resolver(Donation)
 export default class DonationResolver {
@@ -11,8 +13,12 @@ export default class DonationResolver {
     return DonationRepository.getDonations();
   }
 
+  @Authorized()
   @Mutation(() => Donation)
-  createDonation(@Arg('amount') amount: number): Promise<Donation> {
-    return DonationRepository.createDonation(amount);
+  createDonation(
+    @Arg('amount') amount: number,
+    @Ctx() context: GlobalContext
+  ): Promise<Donation> {
+    return DonationRepository.createDonation(amount, context.user as AppUser);
   }
 }
