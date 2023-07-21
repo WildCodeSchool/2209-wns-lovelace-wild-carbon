856b684e7bf4ccc511781eacc01b3c58d0a22d27
Quentin.J <quentin.jouanne94@gmail.com>
2023-07-21 11:45:53 +0200

    error management

--- web-app/src/components/whyCommit/ArticlesRaw.tsx
+++ web-app/src/components/whyCommit/ArticlesRaw.tsx
@@ -29,30 +29,38 @@ interface IArticles {
 
 const ArticlesRaw = () => {
   const { data } = useQuery<QueryQuery>(GET_ARTICLES);
+
   return (
     <>
-      <div className="h-[95vh]">
-        {data?.articles.map((c: IArticles) => (
-          <div
-            key={c.id}
-            className="bg-[#fff] hover:bg-[#f8efe2] duration-300 ease-in-out mb-5 mx-5 p-[30px] rounded-xl cursor-pointer "
-          >
-            <div className="flex flex-col font-bold text-center mb-5">
-              <h2 className="text-[17px]">{c.title}</h2>
-              <br />
-              <p className="text-[13px]">{c.category.categoryName}</p>
+      {data?.articles.length !== 0 ? (
+        <div className="h-[95vh]">
+          {data?.articles.map((c: IArticles) => (
+            <div
+              key={c.id}
+              className="bg-[#fff] hover:bg-[#f8efe2] duration-300 ease-in-out mb-5 mx-5 p-[30px] rounded-xl cursor-pointer "
+            >
+              <div className="flex flex-col font-bold text-center mb-5">
+                <h2 className="text-[17px]">{c.title}</h2>
+                <br />
+                <p className="text-[13px]">{c.category.categoryName}</p>
+              </div>
+              <div className="text-[12px]">{c.description}</div>
             </div>
-            <div className="text-[12px]">{c.description}</div>
-          </div>
-        ))}
-        <Link to={'/register'}>
-          <div className="flex justify-center ">
-            <button className="bg-[#484B8A] hover:bg-[#31335e] duration-300 ease-in-out	 text-[#fff] text-[22px] font-bold rounded-lg w-[75%] py-[10px] mt-5">
-              S'engager
-            </button>
-          </div>
-        </Link>
-      </div>
+          ))}
+          <Link to={'/register'}>
+            <div className="flex justify-center ">
+              <button className="bg-[#484B8A] hover:bg-[#31335e] duration-300 ease-in-out	 text-[#fff] text-[22px] font-bold rounded-lg w-[75%] py-[10px] mt-5">
+                S'engager
+              </button>
+            </div>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex justify-center font-bold text-[#484B8A]">
+          Il n'y a pas d'article pour le moment !
+        </div>
+      )}
+
       <Footer />
     </>
   );
